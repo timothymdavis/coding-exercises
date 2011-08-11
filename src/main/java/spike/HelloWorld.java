4b4fcbe8145c437f6506bc85ba0c4d8f885af4c5
Timothy M. Davis <tmdavis@gmail.com>
2011-08-11 11:25:31 -0400

    Added a class to test JRebel's hotswap setup.

--- src/main/java/spike/HelloWorld.java
+++ src/main/java/spike/HelloWorld.java
@@ -4,6 +4,10 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        // At this point you can modify JRebelHotswapTest and it will hotswap on compilation.
+
+        new JRebelHotswapTest().printSomething();
     }
 
 }
---
+++ src/main/java/spike/JRebelHotswapTest.java
@@ -0,0 +1,11 @@
+package spike;
+
+public class JRebelHotswapTest {
+
+    public void printSomething() {
+        System.out.println("Something has been printed!");
+        System.out.println("Something has been printed!");
+        System.out.println("Something has been printed!");
+    }
+
+}
