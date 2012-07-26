1cfb1bd9672cbd52056d8334a123c5ee028f4c01
Timothy M. Davis <timothy.davis@ihg.com>
2012-07-25 20:26:24 -0400

    Added Calculator class.
    Added a new class that contains a sum method. The sum method takes two
    integers as parameters and returns a long that represents the summation
    of the parameters.

---
+++ dev-process/src/main/java/spike/Calculator.java
@@ -0,0 +1,19 @@
+package spike;
+
+/**
+ * Super simple class for calculating things.
+ */
+public class Calculator
+{
+    /**
+     * Adds two integers together.
+     *
+     * @param left  The first number to add.
+     * @param right The second number to add.
+     * @return The summation of the two parameters.
+     */
+    public long sum(int left, int right)
+    {
+        return left + right;
+    }
+}
