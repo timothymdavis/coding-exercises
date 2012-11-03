4051dcefe94a9e2acbe618cc58263165cd2f9395
Timothy M. Davis <timothy.davis@ihg.com>
2012-11-03 11:28:51 -0400

    Added a simple hash based map implemenation.

---
+++ coding-exercises/src/main/java/spike/hashmap/HashMap.java
@@ -0,0 +1,130 @@
+package spike.hashmap;
+
+/**
+ * A VERY simple hashmap implementation. It handles collisions using the typical bucket solution. It isn't resizable,
+ * so choose your initial size wisely.
+ *
+ * @param <K> The key type.
+ * @param <V> The value type.
+ */
+public class HashMap<K, V> implements Map<K, V> {
+
+    private static final int DEFAULT_SIZE = 1000;
+    private Entry<K, V>[] entries;
+
+    /**
+     * This constructor uses {@link #DEFAULT_SIZE} for map size.
+     */
+    @SuppressWarnings("unchecked")
+    public HashMap() {
+        entries = new Entry[DEFAULT_SIZE];
+    }
+
+    /**
+     * This constructor allows a user to specify the map size.
+     *
+     * Precondition: mapSize > 0
+     *
+     * @param mapSize The initial size of the map.
+     */
+    @SuppressWarnings("unchecked")
+    public HashMap(int mapSize) {
+        if (mapSize < 1) {
+            throw new IllegalArgumentException("Parameter mapSize must be greater than 0!");
+        }
+        entries = new Entry[mapSize];
+    }
+
+    private void assertKeyNotNull(K key) {
+        if (key == null) {
+            throw new IllegalArgumentException("Parameter key must not be null!");
+        }
+    }
+
+    @Override
+    public V get(K key) {
+        assertKeyNotNull(key);
+
+        int indexForKey = getIndexForKey(key);
+        Entry<K, V> bucket = entries[indexForKey];
+
+        while (bucket != null && !bucket.key.equals(key)) {
+            bucket = bucket.next;
+        }
+
+        return bucket == null ? null : bucket.value;
+    }
+
+    private int getIndexForKey(K key) {
+        int entriesLength = entries.length - 1;
+        int maxIndex = entriesLength < 0 ? 0 : entriesLength;
+        return key.hashCode() & maxIndex;
+    }
+
+    @Override
+    public void put(final K key, final V value) {
+        assertKeyNotNull(key);
+
+        Entry<K, V> entry = new Entry<K, V>(key, value);
+        int indexForKey = getIndexForKey(key);
+        Entry<K, V> bucket = entries[indexForKey];
+
+        if (bucket == null) {
+            entries[indexForKey] = entry;
+        }
+
+        while (bucket != null) {
+            if (bucket.key.equals(key)) {
+                entry.next = bucket.next;
+                bucket.next = entry;
+                bucket = null;
+            }
+            else if (bucket.next == null) {
+                bucket.next = entry;
+                bucket = null;
+            }
+            else {
+                bucket = bucket.next;
+            }
+        }
+    }
+
+    @Override
+    public void remove(K key) {
+        assertKeyNotNull(key);
+
+        int indexForKey = getIndexForKey(key);
+        Entry<K, V> bucket = entries[indexForKey];
+        Entry<K, V> prevBucket = null;
+
+        if (bucket != null && bucket.key.equals(key)) {
+            entries[indexForKey] = bucket.next;
+            bucket = null;
+        }
+
+        while (bucket != null) {
+            if (bucket.key.equals(key) && prevBucket != null) {
+                prevBucket.next = bucket.next;
+                bucket = null;
+            }
+            else {
+                prevBucket = bucket;
+                bucket = bucket.next;
+            }
+        }
+    }
+
+    private class Entry<K, V> {
+
+        private K key;
+        private Entry<K, V> next;
+        private V value;
+
+        public Entry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+    }
+
+}
---
+++ coding-exercises/src/test/java/spike/hashmap/HashMapTest.java
@@ -0,0 +1,98 @@
+package spike.hashmap;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNull.nullValue;
+import static org.junit.Assert.assertThat;
+
+public class HashMapTest {
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testGetWithMapSizeOfZero() throws Exception {
+        HashMap<String, String> map = new HashMap<String, String>(0);
+        map.put("some-key", "some-value");
+        map.put("next-key", "next-value");
+        assertThat(map.get("some-key"), is("some-value"));
+        assertThat(map.get("next-key"), is("next-value"));
+    }
+
+    @Test
+    public void testGetWithMapSizeOfOne() throws Exception {
+        HashMap<String, String> map = new HashMap<String, String>(1);
+        map.put("some-key", "some-value");
+        map.put("next-key", "next-value");
+        assertThat(map.get("some-key"), is("some-value"));
+        assertThat(map.get("next-key"), is("next-value"));
+    }
+
+    @Test
+    public void testGet() throws Exception {
+        HashMap<String, String> map = new HashMap<String, String>();
+        map.put("some-key", "some-value");
+        assertThat(map.get("some-key"), is("some-value"));
+    }
+
+    @Test
+    public void testGetWithTwoEntries() throws Exception {
+        HashMap<String, String> map = new HashMap<String, String>();
+        map.put("some-key", "some-value");
+        map.put("next-key", "next-value");
+        assertThat(map.get("some-key"), is("some-value"));
+        assertThat(map.get("next-key"), is("next-value"));
+    }
+
+    @Test
+    public void testGetWithLotsOfEntries() throws Exception {
+        HashMap<String, String> map = new HashMap<String, String>();
+        final int ENTRY_NUM = 1000;
+        for (int i = 0; i < ENTRY_NUM; i++) {
+            map.put("some-key-" + i, "some-value-" + i);
+        }
+        for (int i = 0; i < ENTRY_NUM; i++) {
+            assertThat(map.get("some-key-" + i), is("some-value-" + i));
+        }
+    }
+
+    @Test
+    public void testGetWithLotsOfEntriesMapSizeOfOne() throws Exception {
+        HashMap<String, String> map = new HashMap<String, String>(1);
+        final int ENTRY_NUM = 1000;
+        for (int i = 0; i < ENTRY_NUM; i++) {
+            map.put("some-key-" + i, "some-value-" + i);
+        }
+        for (int i = 0; i < ENTRY_NUM; i++) {
+            assertThat(map.get("some-key-" + i), is("some-value-" + i));
+        }
+    }
+
+    @Test
+    public void testGetFromEmptyMap() throws Exception {
+        HashMap<String, String> map = new HashMap<String, String>();
+        assertThat(map.get("some-key"), nullValue());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testGetWithNullKey() throws Exception {
+        new HashMap<String, String>().get(null);
+    }
+
+    @Test
+    public void testRemoveWithLotsOfEntries() throws Exception {
+        HashMap<String, String> map = new HashMap<String, String>();
+        final int ENTRY_NUM = 1000;
+        for (int i = 0; i < ENTRY_NUM; i++) {
+            map.put("some-key-" + i, "some-value-" + i);
+        }
+        for (int i = 0; i < ENTRY_NUM / 2; i++) {
+            map.remove("some-key-" + i);
+        }
+        for (int i = 0; i < ENTRY_NUM / 2; i++) {
+            assertThat(map.get("some-key-" + i), nullValue());
+        }
+        for (int i = ENTRY_NUM / 2; i < ENTRY_NUM; i++) {
+            assertThat(map.get("some-key-" + i), is("some-value-" + i));
+        }
+    }
+
+}
