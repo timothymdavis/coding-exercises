6df8f016b387726a76768a19e54ba04fa212666a
Tim Davis <tim.davis@messagegears.com>
2016-05-14 15:58:29 -0400

    Added reverse-words

---
+++ src/main/java/spike/reversewords/WordReverser.java
@@ -0,0 +1,55 @@
+package spike.reversewords;
+
+/**
+ * Reverses the words in a snippet of text, but not the punctuation or whitespace.
+ */
+public class WordReverser {
+
+    public static final String IGNORED_CHARACTERS = " .?!,;\t\n";
+
+    private char[] snippet;
+    private int startingCursor;
+    private int endingCursor;
+    
+    public WordReverser(String snippet) {
+        this.snippet = new char[snippet.length()];
+        snippet.getChars(0, snippet.length(), this.snippet, 0);
+        startingCursor = -1;
+        endingCursor = -1;
+    }
+    
+    public String reverseWords() {
+        while (nextWord()) {
+            reverseCurrentWord();
+        }
+        return new String(snippet);
+    }
+    
+    private boolean nextWord() {
+        startingCursor = endingCursor + 1;
+        if (snippet.length <= startingCursor) {
+            return false;
+        }
+        while (IGNORED_CHARACTERS.contains("" + snippet[startingCursor])) {
+            startingCursor++;
+        }
+        endingCursor = startingCursor;
+        while (snippet.length > endingCursor && !IGNORED_CHARACTERS.contains("" + snippet[endingCursor])) {
+            endingCursor++;
+        }
+        return true;
+    }
+    
+    private void reverseCurrentWord() {
+        char[] temp = new char[endingCursor + 1 - startingCursor];
+        int current = 0;
+        for (int i = endingCursor - 1; i >= startingCursor; i--) {
+            temp[current++] = snippet[i];
+        }
+        current = 0;
+        for (int i = startingCursor; i < endingCursor; i++) {
+            snippet[i] = temp[current++];
+        }
+    }
+    
+}
