ed54637bd301b26cdd12d4cac96bb846205bc8f9
Tim Davis <tim.davis@messagegears.com>
2016-05-14 20:00:37 -0400

    Added increment-version

---
+++ src/main/java/spike/incrementversion/VersionIncrementer.java
@@ -0,0 +1,25 @@
+package spike.incrementversion;
+
+import java.util.Stack;
+
+/**
+ * Increment a version number by some amount (e.g. 2.4.9 would become 2.5.0.)
+ */
+public class VersionIncrementer {
+    
+    public static void incrementVersion(Stack<Integer> version, int amountToIncrement) {
+        if (version.isEmpty()) {
+            version.push(amountToIncrement);
+        }
+        else {
+            int incremented = version.pop() + amountToIncrement;
+            if (incremented > 9) {
+                incrementVersion(version, incremented / 10);
+                version.push(incremented % 10);
+            }
+            else {
+                version.push(incremented);
+            }
+        }
+    }
+}
