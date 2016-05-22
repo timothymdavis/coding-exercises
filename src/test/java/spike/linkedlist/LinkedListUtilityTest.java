d6f19b66455c411e05dca78ca5822359a36944cc
Tim Davis <tim.davis@messagegears.com>
2016-05-21 20:54:45 -0400

    Added linkedlist middle element search

---
+++ src/test/java/spike/linkedlist/LinkedListUtilityTest.java
@@ -0,0 +1,52 @@
+package spike.linkedlist;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static spike.linkedlist.LinkedListUtility.LinkedList;
+
+public class LinkedListUtilityTest {
+
+    @Test(expected = NullPointerException.class)
+    public void testGetMiddleValueWithNullEntry() throws Exception {
+        LinkedListUtility.getMiddleValue(null);
+    }
+
+    @Test
+    public void testGetMiddleValueWithOneEntry() throws Exception {
+        LinkedList<String> list = LinkedListUtility.createLinkedList("a");
+        assertThat(LinkedListUtility.getMiddleValue(list), is("a"));
+    }
+
+    @Test
+    public void testGetMiddleValueWithTwoEntries() throws Exception {
+        LinkedList<String> list = LinkedListUtility.createLinkedList("a", "b");
+        assertThat(LinkedListUtility.getMiddleValue(list), is("a"));
+    }
+
+    @Test
+    public void testGetMiddleValueWithThreeEntries() throws Exception {
+        LinkedList<String> list = LinkedListUtility.createLinkedList("a", "b", "c");
+        assertThat(LinkedListUtility.getMiddleValue(list), is("b"));
+    }
+
+    @Test
+    public void testGetMiddleValueWithFourEntries() throws Exception {
+        LinkedList<String> list = LinkedListUtility.createLinkedList("a", "b", "c", "d");
+        assertThat(LinkedListUtility.getMiddleValue(list), is("b"));
+    }
+
+    @Test
+    public void testGetMiddleValueWithNineEntries() throws Exception {
+        LinkedList<String> list = LinkedListUtility.createLinkedList("a", "b", "c", "d", "e", "f", "g", "h", "i");
+        assertThat(LinkedListUtility.getMiddleValue(list), is("e"));
+    }
+
+    @Test
+    public void testGetMiddleValueWithTenEntries() throws Exception {
+        LinkedList<String> list = LinkedListUtility.createLinkedList("a", "b", "c", "d", "e", "f", "g", "h", "i", "j");
+        assertThat(LinkedListUtility.getMiddleValue(list), is("e"));
+    }
+
+}
