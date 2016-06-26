7a5b3b836c2d9fd497eb144f8d2d86708a7ef3cf
Tim Davis <tim.davis@messagegears.com>
2016-06-25 22:03:05 -0400

    Added HackerRank boilerplate class

---
+++ src/main/java/Solution.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+/**
+ * For HackerRank (hackerrank.com) problems. Needs to be in the default package for easy copy/paste.
+ */
+public class Solution {
+
+
+    public static void solve(int[] n) {
+    }
+
+
+    public static void main(String[] args) {
+        Boilerplate.intArrayTestCase(Solution::solve);
+    }
+
+    /**
+     * Boilerplate code for HackerRank problems.
+     */
+    private final static class Boilerplate {
+
+        public static void genericTestCase(TestCaseHandler handler) {
+            Scanner scanner = new Scanner(System.in);
+            int testCaseCount = scanner.nextInt();
+            IntStream.range(0, testCaseCount).forEach(i -> handler.handle(scanner));
+        }
+
+        @SuppressWarnings("unused")
+        public static void intTestCase(IntTestCaseHandler handler) {
+            genericTestCase(s -> handler.handle(s.nextInt()));
+        }
+
+        @SuppressWarnings("unused")
+        public static void twoIntTestCase(TwoIntTestCaseHandler handler) {
+            genericTestCase(s -> handler.handle(s.nextInt(), s.nextInt()));
+        }
+
+        @SuppressWarnings("unused")
+        public static void intArrayTestCase(IntArrayTestCaseHandler handler) {
+            genericTestCase(s -> handler
+                    .handle(IntStream.range(0, s.nextInt())
+                            .map(i -> s.nextInt())
+                            .toArray()));
+        }
+
+        public interface TestCaseHandler {
+            void handle(Scanner scanner);
+        }
+
+        public interface IntTestCaseHandler {
+            void handle(int number);
+        }
+
+        public interface TwoIntTestCaseHandler {
+            void handle(int numberOne, int numberTwo);
+        }
+
+        public interface IntArrayTestCaseHandler {
+            void handle(int[] array);
+        }
+
+    }
+
+}
