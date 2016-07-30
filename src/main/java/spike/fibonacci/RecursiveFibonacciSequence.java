b5b5f175fe85ba1b3127166cf51ab39327d83715
Tim Davis <tim.davis@messagegears.com>
2016-07-30 11:21:01 -0400

    Slimed down the recursive fibonacci solution

--- src/main/java/spike/fibonacci/RecursiveFibonacciSequence.java
+++ src/main/java/spike/fibonacci/RecursiveFibonacciSequence.java
@@ -7,11 +7,8 @@ public class RecursiveFibonacciSequence implements FibonacciSequence {
 
     @Override
     public long fib(long max) {
-        if (max < 1) {
-            return 0;
-        }
-        else if (max == 1) {
-            return 1;
+        if (max <= 1) {
+            return max;
         }
         else {
             return fib(max - 1) + fib(max - 2);
