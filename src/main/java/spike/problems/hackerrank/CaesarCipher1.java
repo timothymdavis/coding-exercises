5b0be41c2224edc93906d1fdccf7579a2240fd97
Tim Davis <tim.davis@messagegears.com>
2016-07-31 11:27:00 -0400

    Reworked CaesarCipher1

--- src/main/java/spike/problems/hackerrank/CaesarCipher1.java
+++ src/main/java/spike/problems/hackerrank/CaesarCipher1.java
@@ -1,41 +1,31 @@
 package spike.problems.hackerrank;
 
-import java.util.Arrays;
+import java.util.function.IntUnaryOperator;
 import java.util.regex.Pattern;
+
+import static java.lang.Character.toChars;
 
 /**
  * See https://www.hackerrank.com/challenges/caesar-cipher-1 and src/main/resources/hackerrank/caesar-cipher-1-English.pdf
  */
 public class CaesarCipher1 {
 
-    private static final Pattern IS_LETTER_PATTERN = Pattern.compile("[a-zA-Z]");
+    private static final Pattern PATTERN = Pattern.compile("[a-zA-Z]");
 
-    public static void solve(String toEncrypt, int rotateCount) {
-        String encrypted = Arrays.stream(toEncrypt.split(""))
-                .map(s -> s.charAt(0))
-                .map(c -> rot(c, rotateCount))
-                .map(String::valueOf)
+    public static void solve(String toEncrypt, int k) {
+        String encrypted = toEncrypt.codePoints()
+                .map(rotateCharacterBy(k))
+                .mapToObj(c -> String.valueOf(toChars(c)))
                 .reduce((a, b) -> a + b)
                 .orElse("");
         System.out.println(encrypted);
     }
 
-    private static char rot(char c, int rotateCount) {
-        if (isLetter(c)) {
-            boolean isUpper = Character.isUpperCase(c);
-            int base = isUpper ? 65 : 97;
-            return (char) (base + (c - base + rotateCount) % 26);
-        }
-        else {
-            return c;
-        }
-    }
-
-    /**
-     * Apparently Character#isLetter(char) won't work for this problem.
-     */
-    private static boolean isLetter(char codePoint) {
-        return IS_LETTER_PATTERN.matcher(String.valueOf(codePoint)).find();
+    private static IntUnaryOperator rotateCharacterBy(int k) {
+        return c -> {
+            int base = Character.isUpperCase(c) ? 65 : 97;
+            return PATTERN.matcher(String.valueOf(toChars(c))).matches() ? base + (c - base + k) % 26 : c;
+        };
     }
 
 }
