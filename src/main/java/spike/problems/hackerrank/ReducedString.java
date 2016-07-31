31c1abc680155a393be2137ee5ebb86deb347dd2
Tim Davis <tim.davis@messagegears.com>
2016-07-30 21:51:33 -0400

    Added an O(n) version of ReducedString

--- src/main/java/spike/problems/hackerrank/ReducedString.java
+++ src/main/java/spike/problems/hackerrank/ReducedString.java
@@ -1,29 +1,25 @@
 package spike.problems.hackerrank;
 
+import java.util.Stack;
+
 /**
  * See https://www.hackerrank.com/challenges/reduced-string and src/main/resources/hackerrank/reduced-string-English.pdf
  */
 public class ReducedString {
 
     public static void solve(String s) {
-        String reduced = reduce(s);
-        System.out.println(reduced.isEmpty() ? "Empty String" : reduced);
-    }
-
-    public static String reduce(String s) {
-        if (s.length() > 2) {
-            String l = reduce(s.substring(0, s.length() / 2));
-            String r = reduce(s.substring(s.length() / 2, s.length()));
-            if (!l.isEmpty() && !r.isEmpty() && l.charAt(l.length() - 1) == r.charAt(0)) {
-                return reduce(l.substring(0, l.length() - 1) + r.substring(1));
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (!stack.isEmpty() && c == stack.peek()) {
+                stack.pop();
             } else {
-                return l + r;
+                stack.push(c);
             }
-        } else if (s.length() == 2 && s.charAt(0) == s.charAt(1)) {
-            return "";
-        } else {
-            return s;
         }
+        System.out.println(stack.stream()
+                .map(String::valueOf)
+                .reduce((a, b) -> a + b)
+                .orElse("Empty String"));
     }
 
 }
